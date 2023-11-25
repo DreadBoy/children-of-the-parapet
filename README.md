cd34394fca2f7b6a41a4deddbb5981ec094191bd
Matic Leva <vertical3life@gmail.com>
2023-11-25 22:57:50 +0100

    Add character animation

---
+++ README.md
@@ -0,0 +1,23 @@
+# Children of the Parapet
+
+## Plan
+
+- [x] Player character
+	- [x] model
+	- [x] controller (move + attack)
+	- [x] animation
+- [ ] Boss
+	- [ ] concept / attack gimmick
+	- [ ] model
+	- [ ] logic (movement + that single attack)
+	- [ ] animation
+- [ ] Simple level
+	- [ ] teach movement
+	- [ ] teach attack (can be done by destroying barrier / gate1)
+	- [ ] enter arena
+- [ ] Arena
+	- [ ] fight
+	- [ ] end screen with link to survey
+- [ ] online survey
+- [ ] main menu
+	- [ ] with warning about survey at the end
--- character/character.gd
+++ character/character.gd
@@ -6,12 +6,15 @@ extends CharacterBody3D
 @export var fall_acceleration = 75
 
 var target_velocity = Vector3.ZERO
+var state_machine
 
+func _ready():
+	state_machine = $Pivot/character/AnimationTree["parameters/playback"]
+	state_machine.travel("idle")
+	
 func _physics_process(delta):
-	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
 
-	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
 		direction.z -= 1
 	if Input.is_action_pressed("move_left"):
@@ -20,19 +23,24 @@ func _physics_process(delta):
 		direction.x -= 1
 	if Input.is_action_pressed("move_down"):
 		direction.x += 1
-
+		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		$Pivot.look_at(position + direction, Vector3.UP)
-		
-		# Ground Velocity
+	
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
 
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	else:
+		target_velocity.y = 0
 
-	# Moving the Character
 	velocity = target_velocity
+	
+	$Pivot.look_at(position + direction, Vector3.UP)
 	move_and_slide()
+
+	if direction != Vector3.ZERO and state_machine.get_current_node() != "walk":
+		state_machine.travel("walk")
+	elif direction == Vector3.ZERO and state_machine.get_current_node() != "idle":
+		state_machine.travel("idle")
